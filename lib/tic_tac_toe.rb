6ea112b61332b0f8f8a06bfa8eafd9e4541ed481
Lluvia Frias <lluvi.frias@gmail.com>
2018-04-25 01:57:37 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -52,5 +52,14 @@ class TicTacToe
     display_board(@board)
   end
 
+  def turn_count
+  counter = 0
+  @board.each do |space|
+    if space == "X" || space == "O"
+    counter += 1
+    end
+  end
+  return counter
+  end
 
 end
