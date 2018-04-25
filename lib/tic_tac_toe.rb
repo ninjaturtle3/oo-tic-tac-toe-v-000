3cba2f880c5d97aafcedeada040caa18bf3d24ca
Lluvia Frias <lluvi.frias@gmail.com>
2018-04-25 01:39:41 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -37,9 +37,9 @@ class TicTacToe
     !(@board[index].nil? || @board[index] == " ")
   end
 
-  def valid_move?(board, index)
-  index.between?(0,8) && !position_taken?(board, index)
-end
+  def valid_move?(index)
+  index.between?(0,8) && !position_taken?(@board, index)
+  end
 
 
 end
